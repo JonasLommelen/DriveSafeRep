4098e61e37ee7f1a52878714bb8bde5028634c03
Jonas Lommelen <jonas.lommelen@student.uhasselt.be>
2018-03-28 10:21:50 +0200

    Eerste commit

---
+++ app/src/main/java/com/example/jonaslommelen/drivesafe/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.jonaslommelen.drivesafe;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.jonaslommelen.drivesafe.utilities.URLBuilder;
+
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText mSearchBoxEditText;
+    private TextView mDisplayUrl;
+    private Button mMaakUrlButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mSearchBoxEditText = (EditText) findViewById(R.id.et_search_box);
+        mDisplayUrl = (TextView) findViewById(R.id.url_display);
+        mMaakUrlButton = (Button) findViewById(R.id.make_url);
+
+        mMaakUrlButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                makeBreweryDBSearchQuery();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int selectedMenuOption = item.getItemId();
+        if(selectedMenuOption == R.id.action_search){
+            makeBreweryDBSearchQuery();
+        }
+        else if (selectedMenuOption == R.id.action_settings){
+            Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
+            String string = mSearchBoxEditText.getText().toString();
+            intent.putExtra(Intent.EXTRA_TEXT, string);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void makeBreweryDBSearchQuery(){
+        String githubSearchQuery = mSearchBoxEditText.getText().toString();
+        URL githubSearchURL = URLBuilder.buildUrl(githubSearchQuery);
+        mDisplayUrl.setText(githubSearchURL.toString());
+    }
+}
---
+++ app/src/main/java/com/example/jonaslommelen/drivesafe/utilities/JsonUtils.java
@@ -0,0 +1,75 @@
+package com.example.jonaslommelen.drivesafe.utilities;
+
+import android.content.Context;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.HttpURLConnection;
+
+/**
+ * Created by Jonas Lommelen on 21/03/2018.
+ */
+
+public class JsonUtils {
+    public static String[] getBeerStringsFromJson(Context context, String beerJsonStr)
+            throws JSONException {
+
+        final String BDB_DATA = "data";
+
+        final String BDB_ID = "id";
+        final String BDB_NAME = "name";
+        final String BDB_NAMEDISPLAY = "nameDisplay";
+
+        final String BDB_PERCENTAGE = "abv";
+        //final String BDB_BITTERNESS = "ibu";
+
+        final String BDB_LABEL = "labels";
+        final String BDB_LABEL_ICON = "icon";
+        final String BDB_LABEL_MEDIUM = "medium";
+        final String BDB_LABEL_LARGE = "large";
+
+        String[] parsedBeerData = null;
+
+        JSONObject beerObject = new JSONObject(beerJsonStr);
+
+//        /* Is there an error? */
+//        if (beerObject.has(OWM_MESSAGE_CODE)) {
+//            int errorCode = beerObject.getInt(OWM_MESSAGE_CODE);
+//
+//            switch (errorCode) {
+//                case HttpURLConnection.HTTP_OK:
+//                    break;
+//                case HttpURLConnection.HTTP_NOT_FOUND:
+//                    /* Location invalid */
+//                    return null;
+//                default:
+//                    /* Server probably down */
+//                    return null;
+//            }
+//        }
+
+        JSONArray beerArray = beerObject.getJSONArray(BDB_DATA);
+
+        parsedBeerData = new String[beerArray.length()];
+
+        for (int i = 0; i < beerArray.length(); i++) {
+
+            JSONObject beerJson = beerArray.getJSONObject(i);
+            JSONObject labelJson = beerJson.getJSONObject(BDB_LABEL);
+
+            String name = beerJson.getString(BDB_NAME);
+            String percentage = beerJson.getString(BDB_PERCENTAGE);
+            String labelPng = null;
+            if (labelJson != null){
+                labelPng = labelJson.getString(BDB_LABEL_ICON);
+            }
+
+            parsedBeerData[i] = name + percentage + labelPng;
+        }
+
+        return parsedBeerData;
+
+    }
+}
